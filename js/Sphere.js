7752c6caa16b56f7debdce9a68e3a39bcb8a4733
elekit <kit93@hotmail.it>
2017-11-25 10:46:09 +0100

    earth sphere added with texture

--- js/Sphere.js
+++ js/Sphere.js
@@ -3,19 +3,6 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 camera.position.z = 50;
 
-/*
-controls = new THREE.TrackballControls( camera );
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 0.8;
-controls.noZoom = false;
-controls.noPan = false;
-controls.staticMoving = true;
-controls.dynamicDampingFactor = 0.3;
-controls.keys = [ 65, 83, 68 ];
-controls.addEventListener( 'change', render );
-
-*/
 
 
 
@@ -37,6 +24,53 @@ renderer.setClearColor( 0xd2d2d2, 1 );
 
 document.body.appendChild( renderer.domElement );
 
+
+
+//Load planet texture
+var texture = new THREE.TextureLoader().load( 'planets_textures/2k_earth_daymap.jpg');
+
+
+//SphereBufferGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
+var geometry = new THREE.SphereBufferGeometry( 10, 20, 20, 0, 6.3, 0, 3.1 );
+var material = new THREE.MeshPhongMaterial( { map: texture, overdraw: 0.5 } );
+var earth = new THREE.Mesh( geometry, material );
+earth.position.set(5, 0, -1);
+
+earth.castShadow = true; //default is false
+earth.receiveShadow = false; //default
+
+scene.add( earth );
+
+
+
+scene.add(new THREE.AmbientLight(0x333333));
+
+//TODO  light must be in the sun position
+
+var light = new THREE.DirectionalLight( 0xffffff, 1);
+light.position.set(-200,200,200);
+scene.add(light);
+
+
+
+
+
+
+var render = function () {
+    requestAnimationFrame( render );
+
+    earth.rotation.x += 0.005;
+    earth.rotation.y += 0.005;
+
+    renderer.render( scene, camera );
+
+};
+
+
+
+render();
+
+
 //light in the scene
 /*
 var lights = [];
@@ -51,30 +85,10 @@ scene.add( lights[ 1 ] );
 scene.add( lights[ 2 ] );
 */
 
-//Load planet texture
-/*var loader = new THREE.TextureLoader();
-loader.load( ' here goes the png ',
-    function (texture){
-        var geometry = new THREE.SphereGeometry( 200, 20, 20 );
-        var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-        var mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-    });
-*/
 
 
-scene.add(new THREE.AmbientLight(0x333333));
+/* sphere in the scene
 
-//TODO  light must be in the sun position
-
-var light = new THREE.DirectionalLight( 0xffffff, 1);
-light.position.set(150,130,-15);
-scene.add(light);
-
-
-
-
-//SphereBufferGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
 var geometry = new THREE.SphereBufferGeometry(10, 20, 20, 0, 6.3, 0, 3.1);
 
 var material = new THREE.MeshPhongMaterial( {color: 0xffe1dc,
@@ -83,14 +97,14 @@ var material = new THREE.MeshPhongMaterial( {color: 0xffe1dc,
     fog: true} );
 
 var sphere = new THREE.Mesh( geometry, material );
-sphere.position.set(10, 10, 1);
+sphere.position.set(50, 50, 1);
 
 sphere.castShadow = true; //default is false
 sphere.receiveShadow = false; //default
 
 scene.add( sphere);
 
-//SphereBufferGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
+
 var geometry1 = new THREE.SphereBufferGeometry(10, 20, 20, 0, 6.3, 0, 3.1);
 
 var material1 = new THREE.MeshPhongMaterial( {color: 0xffe1dc,
@@ -98,30 +112,11 @@ var material1 = new THREE.MeshPhongMaterial( {color: 0xffe1dc,
     vertexColors: THREE.NoColors,
     fog: true} );
 var sphere1 = new THREE.Mesh( geometry1, material1 );
-sphere1.position.set(-10, -10, -1);
+sphere1.position.set(-50, -50, -1);
 
 sphere1.castShadow = true; //default is false
 sphere1.receiveShadow = false; //default
 
 scene.add( sphere1);
 
-
-
-
-var render = function () {
-    requestAnimationFrame( render );
-
-    sphere.rotation.x += 0.005;
-    sphere.rotation.y += 0.005;
-    sphere1.rotation.x += 0.005;
-    sphere1.rotation.y += 0.005;
-
-    renderer.render( scene, camera );
-
-};
-
-
-
-render();
-
-
+*/
