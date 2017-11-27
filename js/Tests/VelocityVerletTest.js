618ec145db66785f163a9ac282ab3cce3111c08c
elekit <kit93@hotmail.it>
2017-11-27 11:36:33 +0100

    code cleanup and test files ordered in right folders

---
+++ js/Tests/VelocityVerletTest.js
@@ -0,0 +1,63 @@
+THREE.Vector3.prototype.toString = function() {
+    return '(' + this.x.toFixed(3) + ', ' + this.y.toFixed(3) + ', ' + this.z.toFixed(3) + ')';
+};
+
+function main() {
+
+    canvasHolder = document.getElementById( 'webgl' );
+    context = canvasHolder.getContext('2d');
+
+    context.fillStyle = 'white';
+    var w = canvasHolder.width, h = canvasHolder.height;
+    context.fillRect( 0, 0, w, h);
+
+    //context.globalCompositeOperation='copy';
+
+    // -------------
+
+    var sun = new CelestialBody('Sun', 100, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0));
+    var earth = new CelestialBody('Earth', 1, new THREE.Vector3(1.0, 0, 0), new THREE.Vector3(0, -10, 0));
+
+    var solarSys = new SolarSystem([sun, earth]);
+
+    solarSys._G = 1.0;
+
+    let acc = solarSys.acceleration(earth, earth.position);
+
+    console.log('acceleration' + acc.x.toFixed(3) + ' ' + acc.y.toFixed(3) + ' ' + acc.z.toFixed(3));
+
+    /*
+        T = 2pi/10= 0,6283185307 , sqrt(M/r * G) = v,  M = 100, m = 1 , r = 1, G = 1
+     */
+
+    const dt = 0.001;
+    var positions = [];
+
+    for (let i = 0; i < 628; i++) {
+
+
+        solarSys.velocity_verlet(earth, dt);
+        console.log('t= ' + (i+1) + '\n earth pos ' + earth.position.toString() + ' length=' + earth.position.length().toFixed(3) +
+            '\n earth vel ' + earth.velocity.toString() + ' length=' + earth.velocity.length().toFixed(3) +
+            ' \n sun pos ' + sun.position.toString() + ' length=' + sun.position.length().toFixed(3));
+
+        positions.push([earth.position.x, earth.position.y]);
+    }
+
+    context.beginPath();
+    //context.moveTo(positions[0][0]*100+200, positions[0][1]*100+200);
+    for(const p of positions)
+        context.lineTo(p[0]*100+200, p[1]*100+200);
+    context.lineWidth = 5;
+    context.strokeStyle = 'blue';
+    context.stroke();
+    //context.closePath();
+
+    // Sun
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.strokeRect(sun.position.x*100+200, sun.position.y*100+200, 1,1);
+    context.stroke();
+    //context.closePath();
+
+}
