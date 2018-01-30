6559a1a5ccf471b910ba9ce67000c2b9f00a78aa
elekit <kit93@hotmail.it>
2018-01-30 21:01:18 +0100

    changed planets material into lambert_material

--- js/Planets_Movement.js
+++ js/Planets_Movement.js
@@ -12,7 +12,7 @@ var planets = {
 
     'sun': {
         'geometry': new THREE.SphereBufferGeometry(SUN.RADIUS * radius_scale * 0.02, SUN.WIDTH, SUN.HEIGHT, SUN.PHI_START, SUN.PHI_LENGTH, SUN.THETA_STARTS, SUN.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_sun.jpg'),
             overdraw: 0.5,
             emissive: 0xFF8100,
@@ -26,10 +26,10 @@ var planets = {
     
     'earth': {
         'geometry': new THREE.SphereBufferGeometry(EARTH.RADIUS * radius_scale, EARTH.WIDTH, EARTH.HEIGHT, EARTH.PHI_START, EARTH.PHI_LENGTH, EARTH.THETA_STARTS, EARTH.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             color: 0xf7f7f7,
             map: new THREE.TextureLoader().load( 'planets_textures/2k_earth_daymap.jpg'),
-            overdraw: 0.5, shininess: 5}),
+            overdraw: 0.5, }),
         'angle' : 0.40910518,
         'rotation_rate' : 7.292115e-5,
         'physics': new CelestialBody('Earth', EARTH.MASS, EARTH.INITIAL_POSITION, EARTH.INITIAL_VELOCITY)
@@ -38,10 +38,10 @@ var planets = {
     
     'moon': {
         'geometry': new THREE.SphereBufferGeometry(MOON.RADIUS * radius_scale, MOON.WIDTH, MOON.HEIGHT, MOON.PHI_START, MOON.PHI_LENGTH, MOON.THETA_STARTS, MOON.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_moon.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.116588,
         'rotation_rate' :  2.6616995e-6,
         'physics': new CelestialBody('Moon', MOON.MASS, MOON.INITIAL_POSITION, MOON.INITIAL_VELOCITY)
@@ -50,10 +50,10 @@ var planets = {
 
     'mercury': {
         'geometry': new THREE.SphereBufferGeometry(MERCURY.RADIUS * radius_scale, MERCURY.WIDTH, MERCURY.HEIGHT, MERCURY.PHI_START, MERCURY.PHI_LENGTH, MERCURY.THETA_STARTS, MERCURY.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_mercury.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 5.235988e-5,
         'rotation_rate' : 0.000001239932688,
         'physics': new CelestialBody('Mercury', MERCURY.MASS, MERCURY.INITIAL_POSITION, MERCURY.INITIAL_VELOCITY)
@@ -62,10 +62,10 @@ var planets = {
     
     'mars': {
         'geometry': new THREE.SphereBufferGeometry(MARS.RADIUS * radius_scale, MARS.WIDTH, MARS.HEIGHT, MARS.PHI_START, MARS.PHI_LENGTH, MARS.THETA_STARTS, MARS.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_mars.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.43964844,
         'rotation_rate' : 0.00007094834358,
         'physics': new CelestialBody('Mars', MARS.MASS, MARS.INITIAL_POSITION, MARS.INITIAL_VELOCITY)
@@ -74,10 +74,10 @@ var planets = {
 
     'venus': {
         'geometry': new THREE.SphereBufferGeometry(VENUS.RADIUS * radius_scale, VENUS.WIDTH, VENUS.HEIGHT, VENUS.PHI_START, VENUS.PHI_LENGTH, VENUS.THETA_STARTS, VENUS.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_venus_atmosphere.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.04607669,
         'rotation_rate' : -0.000000299242049,
         'physics': new CelestialBody('Venus', VENUS.MASS, VENUS.INITIAL_POSITION, VENUS.INITIAL_VELOCITY)
@@ -86,10 +86,10 @@ var planets = {
     
     'jupiter': {
         'geometry': new THREE.SphereBufferGeometry(JUPITER.RADIUS * radius_scale, JUPITER.WIDTH, JUPITER.HEIGHT, JUPITER.PHI_START, JUPITER.PHI_LENGTH, JUPITER.THETA_STARTS, JUPITER.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_jupiter.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.05462881,
         'rotation_rate' : 1.75865e-4,
         'physics': new CelestialBody('Jupiter', JUPITER.MASS, JUPITER.INITIAL_POSITION, JUPITER.INITIAL_VELOCITY)
@@ -98,10 +98,10 @@ var planets = {
     
     'uranus': {
         'geometry': new THREE.SphereBufferGeometry(URANUS.RADIUS * radius_scale, URANUS.WIDTH, URANUS.HEIGHT, URANUS.PHI_START, URANUS.PHI_LENGTH, URANUS.THETA_STARTS, URANUS.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_uranus.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 1.4351842,
         'rotation_rate' : -0.0001014726309,
         'physics': new CelestialBody('Uranus', URANUS.MASS, URANUS.INITIAL_POSITION, URANUS.INITIAL_VELOCITY)
@@ -109,10 +109,10 @@ var planets = {
     
     'neptune': {
         'geometry': new THREE.SphereBufferGeometry(NEPTUNE.RADIUS * radius_scale, NEPTUNE.WIDTH, NEPTUNE.HEIGHT, NEPTUNE.PHI_START, NEPTUNE.PHI_LENGTH, NEPTUNE.THETA_STARTS, NEPTUNE.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_neptune.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.494277244,
         'rotation_rate' : 1.083e-4,
         'physics': new CelestialBody('Neptune', NEPTUNE.MASS, NEPTUNE.INITIAL_POSITION, NEPTUNE.INITIAL_VELOCITY),
@@ -121,10 +121,10 @@ var planets = {
     'saturn': {
         //SphereBufferGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
         'geometry': new THREE.SphereBufferGeometry(SATURN.RADIUS * radius_scale, SATURN.WIDTH, SATURN.HEIGHT, SATURN.PHI_START, SATURN.PHI_LENGTH, SATURN.THETA_STARTS, SATURN.THETA_LENGTH),
-        'material': new THREE.MeshPhongMaterial({
+        'material': new THREE.MeshLambertMaterial({
             map: new THREE.TextureLoader().load( 'planets_textures/2k_saturn.jpg'),
             overdraw: 0.5,
-            shininess: 5}),
+            }),
         'angle' : 0.46652651,
         'rotation_rate' : 1.63785e-4,
         'physics': new CelestialBody('Saturn', SATURN.MASS, SATURN.INITIAL_POSITION, SATURN.INITIAL_VELOCITY),
